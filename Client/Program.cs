4eb4e8295f5a79ea1377074a2816e0e4e98d9833
marcinpl500 <marcin.gardy@hotmail.com>
2023-03-10 22:15:27 +0100

    FileTransferApp

---
+++ Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Linq;
+
+
+
+
+namespace FileTranferApplication
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            //System.Console.WriteLine($"PcIP?:{Dns.GetHostAddresses)}");   
+            //var DevicesInNetwork = 
+            
+        }
+    }
+}
